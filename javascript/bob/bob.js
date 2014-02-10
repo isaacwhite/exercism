6595e9fce33b57638c5840df9cafd9f34f7b58c3
Isaac White <iaw2105@columbia.edu>
2014-02-10 03:27:59 -0500

    Converting indentation...

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,57 +3,57 @@ var Bob = function() {
 };
 
 Bob.prototype.hey = function (input) {
-	function testInput(string) {
-		function testEmpty(string) {
-			if((string.length === 0) ||
-			   (string.replace(/ /g,"").length === 0)) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		function testNumbers(string) {
-			if((string === string.toUpperCase()) &&
-			   (string === string.toLowerCase()) &&
-			   (!testEmpty(string))) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		function testShout(string) {
-			if((string === string.toUpperCase()) &&
-			   (string !== string.toLowerCase())) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		function testQuestion(string) {
-			if((!testEmpty(string)) &&
-			   (string.substr(string.length - 1) === "?")) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		return {
-			numbers: testNumbers(string),
-			empty: testEmpty(string),
-			shout: testShout(string),
-			question: testQuestion(string)
-		};
-	}
-	var testResult = testInput(input);
+  function testInput(string) {
+    function testEmpty(string) {
+      if((string.length === 0) ||
+         (string.replace(/ /g,"").length === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    function testNumbers(string) {
+      if((string === string.toUpperCase()) &&
+         (string === string.toLowerCase()) &&
+         (!testEmpty(string))) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    function testShout(string) {
+      if((string === string.toUpperCase()) &&
+         (string !== string.toLowerCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    function testQuestion(string) {
+      if((!testEmpty(string)) &&
+         (string.substr(string.length - 1) === "?")) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return {
+      numbers: testNumbers(string),
+      empty: testEmpty(string),
+      shout: testShout(string),
+      question: testQuestion(string)
+    };
+  }
+  var testResult = testInput(input);
 
-	if(testResult.empty) {
-		return "Fine. Be that way!";
-	} else if(testResult.shout) {
-		return "Woah, chill out!";
-	} else if(testResult.question) {
-		return "Sure.";
-	} else {
-		return "Whatever.";
-	}
+  if(testResult.empty) {
+    return "Fine. Be that way!";
+  } else if(testResult.shout) {
+    return "Woah, chill out!";
+  } else if(testResult.question) {
+    return "Sure.";
+  } else {
+    return "Whatever.";
+  }
 };
 module.exports = Bob;
