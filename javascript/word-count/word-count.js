6c4a8323dd31d8a36ffbc5cebb70b5ecaa4eaf37
Isaac White <iaw2105@columbia.edu>
2014-02-11 23:08:52 -0500

    Counter object for word count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,31 +1,37 @@
 function words(input) {
   "use strict";
-  function getWords(input) {
-    function removePunctuation(string) {
-      return string.replace(/\W+/ig," ").replace(/\s$|^\s/g,"");
-    }
-    function splitStringBySpaces(string) {
-      return string.split(/\s+/g);
-    }
-    function countWords(wordsArray) {
-      var wordCounts, i, currentWord;
-      wordCounts = {};
-      for(i = 0; i<wordsArray.length; i++){
-        currentWord = wordsArray[i].toLowerCase();
-        if(wordCounts[currentWord]) {
-          wordCounts[currentWord]++;
+  function Counter() {
+    var counts = {};
+    return {
+      add: function(word) {
+        var lowerCase = word.toLowerCase();
+        if(counts[lowerCase]) {
+          counts[lowerCase]++;
         } else {
-          wordCounts[currentWord] = 1;
+          counts[lowerCase] = 1;
         }
+      },
+      getCounts: function() {
+        return counts;
       }
-      return wordCounts;
-    }
-    var words, wordCounts;
-    words = removePunctuation(input);
-    words = splitStringBySpaces(words);
-    wordCounts = countWords(words);
-    return wordCounts;
+    };
   }
-  return getWords(input);
+  function removePunctuation(string) {
+    return string.replace(/\W+/g," ").replace(/\s$|^\s/g,"");
+  }
+  function splitStringBySpaces(string) {
+    return string.split(/\s+/g);
+  }
+  function countWords(wordsArray) {
+    var counter, i;
+    counter = new Counter();
+    for(i = 0; i < wordsArray.length; i++){
+      counter.add(wordsArray[i]);
+    }
+    return counter.getCounts();
+  }
+  input = removePunctuation(input);
+  input = splitStringBySpaces(input);
+  return countWords(input);
 }
 module.exports = words;
