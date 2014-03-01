df53f2acc59f4c3deaf2f1b40c950c607d57c635
Isaac White <iaw2105@columbia.edu>
2014-03-01 12:33:46 -0500

    Small tweaks

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,17 @@
 var Bob = function () {}; //empty constructor
 Bob.prototype.hey = function (input) {
   "use strict";
+  
   var test = {
     empty:    input.trim() === "",
     shout:    input === input.toUpperCase() &&
               input !== input.toLowerCase(),
     question: input.slice(-1) === "?"
   };
+
   if(test.empty) return "Fine. Be that way!";
   if(test.shout) return "Woah, chill out!";
-  if(test.question) return "Sure."; else return "Whatever.";
+  if(test.question) return "Sure.";
+  else return "Whatever.";
 };
 module.exports = Bob;
