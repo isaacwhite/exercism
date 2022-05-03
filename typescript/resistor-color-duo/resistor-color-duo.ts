2bc028fe817d26d24d2e8f950ad5c5c2aa311869
Isaac White <hello@isaacwhite.com>
2022-05-02 23:18:58 -0400

    technically not o(1), but shorter

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,38 +1,25 @@
-type BandColors =
-  "black" |
-  "brown" |
-  "red" |
-  "orange" |
-  "yellow" |
-  "green" |
-  "blue" |
-  "violet" |
-  "grey" |
-  "white";
-
-const Bands: Record<BandColors, number> = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9,
-};
+const bandColors = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
 
 export function decodedValue(colors: string[]): number {
   const [first, second] = colors;
-  return Number([first, second].map(c => decodeBand(c)).join(''));
+  return Number([first, second].map(decodeBand).join(""));
 }
 
 function decodeBand(bandColor: string): number {
-  const uppercase = bandColor.toLowerCase();
-  if (uppercase in Bands) {
-    return Bands[uppercase as BandColors];
-  }
+  const lowercase = bandColor.toLowerCase();
+  const position = bandColors.indexOf(lowercase)
+  if (position < 0)  throw Error("Received unexpected band color!");
 
-  throw Error("Received unexpected band color!");
+  return position
 }
